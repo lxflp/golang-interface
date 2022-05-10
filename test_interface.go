f6775d015f8e6ab455621487867262009b86beb0
aleksej <sv0b0da0k0@gmail.com>
2022-05-10 16:50:16 +0900

    добавил примеры интерфэйсов

---
+++ test_interface.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type weighter interface {
+	weigh() int
+}
+
+type scalesV1 struct {
+}
+
+func (s scalesV1) weigh() int {
+	return 85
+}
+
+type scalesV2 struct {
+}
+
+func (s scalesV2) weigh() int {
+	return 100
+}
+
+func conveer(w weighter) int {
+	return w.weigh()
+}
+
+func main1() {
+	var m scalesV1
+	fmt.Println(conveer(m))
+	var p scalesV2
+	fmt.Println(conveer(p))
+}
